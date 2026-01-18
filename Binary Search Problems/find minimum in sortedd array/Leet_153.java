b89a2d8076fbeac3eea3b1f0d4557cf7057fc7ab
Shyam Thakur <er.shyam.work@gmail.com>
2026-01-18 22:46:25 +0530

    Initialize Java DSA practice repository with basic structure

---
+++ Binary Search Problems/find minimum in sortedd array/Leet_153.java
@@ -0,0 +1,29 @@
+// leetcode 153 - find minimum in sortedd array.
+
+public class Leet_153 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,5,6,7,0,1,2};
+
+        int ans = findMin(nums);
+        System.out.println(ans);
+    }
+
+    static int findMin(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
---
+++ Recursion Problems/Fibonacci Number/Fibonacci_Number.java
@@ -0,0 +1,12 @@
+class Fibonacci_Number{
+    public static void main(String[] args) {
+        int x=fibo(6);
+        System.out.println(x);
+    }
+    static int fibo(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
