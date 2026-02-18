20ae103cf122628da50b41a82bdfb904704965e1
Shyam Thakur <er.shyam.work@gmail.com>
2026-02-19 01:03:44 +0530

    Permutations and Google , Amazon Question

---
+++ Recursion Problems/Permutations/code.java
@@ -0,0 +1,59 @@
+
+import java.util.ArrayList;
+
+public class code{
+    public static void main(String[]args){
+        permutations("","abc");
+        ArrayList<String> ans = permutationsList("","abc");
+        System.out.println(ans);
+        System.out.println(permutationscount("","abc"));
+    }
+    static void permutations(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+            for(int i=0;i<=p.length();i++){
+                String f=p.substring(0,i);
+                String s=p.substring(i,p.length());
+                permutations(f+ch+s,up.substring(1));
+            }
+    }
+
+//output:cba bca bac cab acb abc
+
+
+static ArrayList<String> permutationsList(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+            for(int i=0;i<=p.length();i++){
+                String f=p.substring(0,i);
+                String s=p.substring(i,p.length());
+                ans.addAll(permutationsList(f+ch+s,up.substring(1)));
+            }
+            return ans;
+    }
+    //output:[cba, bca, bac, cab, acb, abc]
+
+    static int permutationscount(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return 1;
+        }
+        int count=0;
+        char ch = up.charAt(0);
+            for(int i=0;i<=p.length();i++){
+                String f=p.substring(0,i);
+                String s=p.substring(i,p.length());
+                count = count+ permutationscount(f+ch+s,up.substring(1));
+            }
+            return count;
+    }
+
+}
